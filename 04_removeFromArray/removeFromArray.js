2127fa753d3ff4949082f866bb308f3c7da31a1f
pyudai <premyuda321@gmail.com>
2022-07-01 17:48:25 +0700

    do exercise4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr, ...valueList) {
+  for (let i = 0; i < valueList.length; i++)
+    arr = arr.filter((x) => x !== valueList[i]);
+  return arr;
 };
 
 // Do not edit below this line
