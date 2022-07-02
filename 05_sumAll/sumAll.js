5c629cdb630825e56783f007ac2ae4a926c347e1
pyudai <premyuda321@gmail.com>
2022-07-02 17:40:16 +0700

    do exercise5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+  let result = 0;
 
+  if (start < 0 || end < 0 || typeof end !== 'number') return "ERROR";
+
+  for (
+    let i = start < end ? start : end;
+    start < end ? i <= end : i <= start;
+    i++
+  ) {
+    result += i;
+  }
+  return result;
 };
 
 // Do not edit below this line
