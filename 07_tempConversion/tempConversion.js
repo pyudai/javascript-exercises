827a5c2d332330b6874dc3a29dfc6446b08d17ae
pyudai <premyuda321@gmail.com>
2022-07-02 17:51:11 +0700

    do exercise7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function (degree) {
+  return Math.round((((degree - 32) * 5) / 9) * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function (degree) {
+  return Math.round(((degree * 9) / 5 + 32) * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
