22e7dafb0632adbac21a0df64c018ff4b6b76a7b
pyudai <premyuda321@gmail.com>
2022-07-13 10:04:55 +0700

    do exercise9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (str) {
 
+    let test = str.match(/[A-Z]/gi).join("").toLowerCase();
+  for (let i = 0; i < test.length / 2; i++)
+    if (test[i] !== test[test.length - i - 1]) return false;
+  return true;
 };
 
 // Do not edit below this line
