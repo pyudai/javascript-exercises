0d997dcf3a1e52a586005ece0febba2278956deb
pyudai <premyuda321@gmail.com>
2022-07-13 10:27:38 +0700

    do exercise10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function (order) {
+  let numA = (numB = 1);
+  if (order<0) return "OOPS";
+  if (order === 1 || order === 2) return 1;
+  for (let i = 2; i < order; i++) {
+    let temp = numA;
+    numA = numB;
+    numB = numA + temp;
+  }
+  return numB;
 };
 
 // Do not edit below this line
