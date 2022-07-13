c46557ba59785012b28730bc70b38e9e65ce0e46
pyudai <premyuda321@gmail.com>
2022-07-13 10:49:10 +0700

    do exercise12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  people.map(
+    (p) =>
+      (p.age = p.yearOfDeath
+        ? p.yearOfDeath - p.yearOfBirth
+        : new Date().getFullYear() - p.yearOfBirth)
+  );
+  return people.reduce((acc, val) => {
+    acc = acc.age === undefined || val.age > acc.age ? val : acc;
+    return acc;
+  }, []);
 };
 
 // Do not edit below this line
